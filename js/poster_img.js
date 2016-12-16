fda357cc70841f31b0f7f49643b370e6d15b0712
vemay <396104393@qq.com>
2016-12-16 17:49:02 +0800

    面向对象的JS结构搭建

--- js/poster_img.js
+++ js/poster_img.js
@@ -1,6 +1,17 @@
 /**
  * Created by Administrator on 2016/12/15.
  */
-(function ($) {
-    alert($);
+;(function($) {
+    var Carousel = function (poster) {
+        alert(poster);
+    };
+    Carousel.prototype = {
+
+    };
+    Carousel.init = function (posters) {
+        posters.each(function () {
+            new Carousel(this);
+        });
+    }
+    window["Carousel"] = Carousel;
 })(jQuery);
