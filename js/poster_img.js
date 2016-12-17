d32fda60bbb8f05808678c6241fbc89bd5d20ec5
vemay <396104393@qq.com>
2016-12-17 10:58:47 +0800

    左侧属性布局结束

--- js/poster_img.js
+++ js/poster_img.js
@@ -18,7 +18,7 @@
             "imgHeight":270,/*广告图第一帧高度*/
             "verticalAlign":"middle",/*垂直排列样式  top middle bottom*/
             "scale":0.9,/*图片层叠缩放比例*/
-            "speed":500,/*切换速度*/
+            "speed":500/*切换速度*/
         };
 
         /*将人工定义的参数与默认参数合并*/
@@ -33,18 +33,17 @@
                 /*广告最外层盒子的属性*/
                 this.poster.css({
                     "width":this.setting.width,
-                    "height":this.setting.height,
+                    "height":this.setting.height
                 });
 
                 /*广告列表ul的属性*/
                 this.list.css({
                     "width":this.setting.width,
-                    "height":this.setting.height,
+                    "height":this.setting.height
                 });
 
                 /*左右按钮的属性*/
                 var w = parseInt((this.setting.width-this.setting.imgWidth)/2);
-                var zindex=0;
                 this.leftBtn.css({
                     "width":w,
                     "height":this.setting.height,
@@ -71,6 +70,7 @@
             setSliceItems:function() {
                 var self        = this,
                     sliceSize   = this.sliceItems.size()/2,
+                    /*右侧剩余帧*/
                     rightSlice  = this.sliceItems.slice(0,sliceSize),
                     rw           = this.setting.imgWidth ,
                     rh           = this.setting.imgHeight,
@@ -78,23 +78,43 @@
                     // level = sliceSize 右边这一组有几张图片就分几层，因此层数与右侧图片数相等
                     gap          = firstLeft/sliceSize;
 
-                    rightSlice.each(function(i,ele) {
+                rightSlice.each(function(i) {
 
                         rw= rw*self.setting.scale;
                         rh= rh*self.setting.scale;
-                        var rleft = firstLeft + self.setting.imgWidth+(i+1)*gap - rw;
-                        console.log(self.setting.height+"   "+rh);
-                        var rtop  = (self.setting.height-rh)/2;
-                        var rOpacity = 1/(i+1);
+                        var rLeft = firstLeft + self.setting.imgWidth+(i+1)*gap - rw,
+                            rTop  = (self.setting.height-rh)/2,
+                            rOpacity = 1/(i+1);
                         $(this).css({
                                 "width":rw,
                                 "height":rh,
                                 "opacity":rOpacity,
-                                "left":rleft,
-                                "top":rtop,
+                                "left":rLeft,
+                                "top":rTop,
                                 "z-index":sliceSize - i
-                            });
+                        });
                     });
+                /*左侧剩余帧*/
+
+               var leftSlice   = this.sliceItems.slice(sliceSize),
+                   /*左侧第一帧的属性等于右侧最后一帧*/
+                   lw           = rightSlice.last().width(),
+                   lh           = rightSlice.last().height();
+
+               leftSlice.each(function (i,ele) {
+                   var lOpacity = 1/(sliceSize-i),
+                       lTop     = (self.setting.height-lh)/2;
+                   $(this).css({
+                       "width":lw,
+                       "height":lh,
+                       "left":i*gap,
+                       "opacity":lOpacity,
+                       "top":lTop,
+                       "z-index":i+1
+                   });
+                   lw   /= self.setting.scale;
+                   lh   /= self.setting.scale;
+               });
              },
 
         /*获取人工定义的属性参数*/
@@ -103,12 +123,20 @@
                 if (setting && setting!=""){
                     return $.parseJSON(setting);
                 }
+            },
+            /*逆序*/
+            reverseItems: function (items) {
+                var size = items.size();
+                items.each(function (i,ele) {
+                    $(this).index(size- i -1) ;
+                });
+
             }
     };
     Carousel.init = function (posters) {
         posters.each(function () {
             new Carousel($(this));
         });
-    }
+    };
     window["Carousel"] = Carousel;
 })(jQuery);
